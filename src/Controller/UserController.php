0f848aaacfc40b06844aca007515aa7cb8feef55
axelle <kadance9720@gmail.com>
2021-10-11 15:03:08 +0200

    Création Edit form account

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,29 +3,57 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\EditAccountType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     public function index(User $user): Response
     {
         return $this->render('user/index.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'post' => $user->getPosts()
         ]);
     }
 
-    public function edit(User $user): Response
+    public function edit(User $user, Request $request): Response
     {
+        $form = $this->createForm(EditAccountType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Votre compte à été modifié avec succès');
+
+            return $this->redirectToRoute('account', ['id' => $user->getId()]);
+        }
+
         return $this->render('user/user_edit.html.twig', [
+            'editAccountForm' => $form->createView(),
             'user' => $user
         ]);
     }
 
     public function delete(User $user): Response
     {
-        return $this->render('user/index.html.twig', [
+        if ($user) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Votre compte à été supprimer avec succès');
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('user/user_delete.html.twig', [
             'user' => $user
         ]);
     }
---
+++ src/Form/EditAccountType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Length;
+
+class EditAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', Emailtype::class)
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre nom d\'utilisateur doit contenir au moins {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
